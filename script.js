9f1ce70c4c39b823d6255aa68003f7c8cac98f0d
mutai-gilbert <mutaigilbert85@gmail.com>
2023-02-14 11:44:26 +0300

    Fixed the bug on the removeBook function on the  script.js file

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
 const books = JSON.parse(localStorage.getItem('books')) || [];
 const bookContainer = document.querySelector('.book-container');
 
-// function removeBook(btn, i) {
-//   // const parentLi = btn.parentNode;
-//   // bookContainer.removeChild(parentLi);
-  
-// }
+function removeBook(btn) {
+  // remove the book from the dom
+  const parentLi = btn.parentNode;
+  bookContainer.removeChild(parentLi);
+  // remove from the local storage
+  const id = btn.getAttribute('data-id');
+  const parseId = parseInt(id, 10);
+  const index = books.findIndex((book) => book.id === parseId);
+  books.splice(index, 1);
+  // update it on the local storage the changes made
+  localStorage.setItem('books', JSON.stringify(books));
+}
 
 function displayBooks() {
   bookContainer.innerHTML = '';
   localStorage.setItem('books', JSON.stringify(books));
 
-  books.forEach((item) => {
+  books.forEach((book) => {
     const li = document.createElement('li');
-    const innerHtml = ` 
-        <span>${item.title}</span>
-        <span>${item.author}</span>
-        <button class="remove-btn">Remove</button>`;
-    li.innerHTML = innerHtml;
-    bookContainer.appendChild(li);
-    const removeBtns = document.querySelectorAll('.remove-btn');
-    [...removeBtns].forEach((btn, i) => {
-      btn.addEventListener('click', () => {
-        books.splice(i, 1);
-        // localStorage.setItem('books', JSON.stringify(books));
-        displayBooks();
-      });
+    const titleSpan = document.createElement('span');
+    titleSpan.innerText = book.title;
+    const authorSpan = document.createElement('span');
+    authorSpan.innerText = book.author;
+
+    const removeBtn = document.createElement('button');
+    removeBtn.innerText = 'Remove';
+    removeBtn.classList.add('remove-btn');
+    removeBtn.setAttribute('data-id', book.id);
+
+    removeBtn.addEventListener('click', () => {
+      removeBook(removeBtn);
     });
+    li.appendChild(titleSpan);
+    li.appendChild(authorSpan);
+    li.appendChild(removeBtn);
+    bookContainer.appendChild(li);
   });
 }
 
@@ -39,7 +49,7 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const title = form.title.value;
   const author = form.author.value;
-  books.push({ title, author });
+  books.push({ title, author, id: Date.now() });
   displayBooks();
   form.reset();
 });
