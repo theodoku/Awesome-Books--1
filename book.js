200559729e2cf4c69e698e4c1b57b0621050a281
BB-Simon <simon.chowdery@gmail.com>
2023-02-14 11:55:26 +0200

    Add book and booklist class

---
+++ book.js
@@ -0,0 +1,7 @@
+export default class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.id = Date.now();
+  }
+}
--- script.js
+++ script.js
@@ -1,55 +1,68 @@
-const books = JSON.parse(localStorage.getItem('books')) || [];
-const bookContainer = document.querySelector('.book-container');
+import Book from './book.js';
 
-function removeBook(btn) {
-  // remove the book from the dom
-  const parentLi = btn.parentNode;
-  bookContainer.removeChild(parentLi);
-  // remove from the local storage
-  const id = btn.getAttribute('data-id');
-  const parseId = parseInt(id, 10);
-  const index = books.findIndex((book) => book.id === parseId);
-  books.splice(index, 1);
-  // update it on the local storage the changes made
-  localStorage.setItem('books', JSON.stringify(books));
-}
+class BookList {
+  constructor() {
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+    this.bookContainer = document.querySelector('.book-container');
+    this.displayBooks();
+  }
 
-function displayBooks() {
-  bookContainer.innerHTML = '';
-  localStorage.setItem('books', JSON.stringify(books));
+  displayBooks() {
+    this.bookContainer.innerHTML = '';
+    this.books.forEach((book) => {
+      this.li = document.createElement('li');
+      this.titleSpan = document.createElement('span');
+      this.titleSpan.innerText = book.title;
+      this.authorSpan = document.createElement('span');
+      this.authorSpan.innerText = book.author;
 
-  books.forEach((book) => {
-    const li = document.createElement('li');
-    const titleSpan = document.createElement('span');
-    titleSpan.innerText = book.title;
-    const authorSpan = document.createElement('span');
-    authorSpan.innerText = book.author;
+      this.removeBtn = document.createElement('button');
+      this.removeBtn.innerText = 'Remove';
+      this.removeBtn.classList.add('remove-btn');
+      this.removeBtn.setAttribute('data-id', book.id);
+      this.removeBtn.addEventListener('click', this.removeBook.bind(this));
 
-    const removeBtn = document.createElement('button');
-    removeBtn.innerText = 'Remove';
-    removeBtn.classList.add('remove-btn');
-    removeBtn.setAttribute('data-id', book.id);
-
-    removeBtn.addEventListener('click', () => {
-      removeBook(removeBtn);
+      this.li.appendChild(this.titleSpan);
+      this.li.appendChild(this.authorSpan);
+      this.li.appendChild(this.removeBtn);
+      this.bookContainer.appendChild(this.li);
     });
-    li.appendChild(titleSpan);
-    li.appendChild(authorSpan);
-    li.appendChild(removeBtn);
-    bookContainer.appendChild(li);
-  });
+  }
+
+  addBook(newBook) {
+    this.books.push(newBook);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
+
+  removeBook() {
+    // remove the book from the dom
+    console.log(this);
+    console.log(this.btn);
+    console.log(this.bookContainer);
+    this.parentLi = this.btn.parentNode;
+    this.bookContainer.removeChild(this.parentLi);
+    // remove from the local storage
+    const id = this.btn.getAttribute('data-id');
+    const parseId = parseInt(id, 10);
+    const index = this.books.findIndex((book) => book.id === parseId);
+    console.log('index', index);
+    console.log('parseId', parseId);
+    this.books.splice(index, 1);
+    // update it on the local storage the changes made
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
 }
 
-displayBooks();
+const booklist = new BookList();
 
 // function to add newbook
-
 const form = document.querySelector('#add-book');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const title = form.title.value;
   const author = form.author.value;
-  books.push({ title, author, id: Date.now() });
-  displayBooks();
+  const newBook = new Book(title, author);
+  booklist.addBook(newBook);
   form.reset();
 });
